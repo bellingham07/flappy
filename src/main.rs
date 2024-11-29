841dad23bcba370011f8f1fd0bab911196352cc8
Bellingham <93235908+bellingham07@users.noreply.github.com>
2024-11-29 16:56:32 +0800

    feat:add player

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,59 @@ enum GameMode {
     End,
 }
 
+const SCREEN_WIDTH: i32 = 80;
+const SCREEN_HEIGHT: i32 = 50;
+const FRAME_DURATION: f32 = 75.0;
 
 struct State {
-    mode:GameMode,
+    mode: GameMode,
+    player: Player,
+    // 经过多少帧之后，累计了多少时间
+    frame_time: f32,
+}
+
+struct Player {
+    x: i32,
+    y: i32,
+    // 重力方向的速度，小于0就会下落
+    velocity: f32,
+}
+
+impl Player {
+    fn new(x: i32, y: i32) -> Self {
+        Player {
+            x: 0,
+            y: 0,
+            velocity: 0.0,
+        }
+    }
+
+    fn render(&mut self, ctx: &mut BTerm) {
+        ctx.set(0, self.y, YELLOW, BLACK, to_cp437('@'));
+    }
+
+    fn gravity_to_move(&mut self) {
+        if self.velocity < 2.0 {
+            self.velocity += 0.2;
+        }
+        self.y += self.velocity as i32;
+        self.x += 1;
+        if self.y < 0 {
+            self.y = 0
+        }
+    }
+
+    fn flap(&mut self) {
+        self.velocity = -2.0;
+    }
 }
 
 impl State {
     fn new() -> Self {
-        State{
-            mode:GameMode::Menu,
+        State {
+            mode: GameMode::Menu,
+            frame_time: 0.0,
+            player: Player::new(5, 25),
         }
     }
 
@@ -24,22 +68,41 @@ impl State {
         ctx.print_centered(8, "(P) Play Game");
         ctx.print_centered(9, "(Q) Quit");
 
-        if let Some (key) = ctx.key {
+        if let Some(key) = ctx.key {
             match key {
-                VirtualKeyCode::P=>self.restart(),
-                VirtualKeyCode::Q=>ctx.quitting = true,
-                _=>{}
+                VirtualKeyCode::P => self.restart(),
+                VirtualKeyCode::Q => ctx.quitting = true,
+                _ => {}
             }
         }
     }
 
     fn restart(&mut self) {
+        self.player = Player::new(5, 25);
+        self.frame_time = 0.0;
         self.mode = GameMode::Playing;
     }
 
     fn play(&mut self, ctx: &mut BTerm) {
-        // todo
-        self.mode = GameMode::End;
+        ctx.cls_bg(NAVY);
+        self.frame_time += ctx.frame_time_ms;
+
+        if self.frame_time > FRAME_DURATION {
+            self.frame_time = 0.0;
+            self.player.gravity_to_move();
+        }
+
+        if let Some(VirtualKeyCode::Space) = ctx.key {
+            self.player.flap();
+        }
+
+        self.player.render(ctx);
+
+        ctx.print(0, 0, "press space to flap");
+
+        if self.player.y > SCREEN_HEIGHT {
+            self.mode = GameMode::End;
+        }
     }
     fn dead(&mut self, ctx: &mut BTerm) {
         // clear the screen
@@ -48,11 +111,11 @@ impl State {
         ctx.print_centered(8, "(P) Play Game");
         ctx.print_centered(9, "(Q) Quit");
 
-        if let Some (key) = ctx.key {
+        if let Some(key) = ctx.key {
             match key {
-                VirtualKeyCode::P=>self.restart(),
-                VirtualKeyCode::Q=>ctx.quitting = true,
-                _=>{}
+                VirtualKeyCode::P => self.restart(),
+                VirtualKeyCode::Q => ctx.quitting = true,
+                _ => {}
             }
         }
     }
@@ -70,8 +133,8 @@ impl GameState for State {
 
 fn main() -> BError {
     let context = BTermBuilder::simple80x50()
-    .with_title("flappy dragon")
-    .build()?;
+        .with_title("flappy dragon")
+        .build()?;
 
     main_loop(context, State::new())
 }
